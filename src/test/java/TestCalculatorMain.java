2c24831736f011d63be369095ca7452516b4e9f5
alsamancov <alsamancov@gmail.com>
2015-11-12 04:27:10 +0200

    second commit

---
+++ src/test/java/TestCalculatorMain.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TestCalculatorMain {
+    @Test
+    public void testGetListFromFile() throws IOException {
+        List<String> testList = CalculatorMain.getListFromFile();
+        String str = testList.get(0);
+
+        assertEquals(str, "apply 1");
+
+        assertNotEquals(str, "");
+
+        assertNotNull(str);
+
+    }
+
+    @Test
+    public void testGetDigits(){}
+}
